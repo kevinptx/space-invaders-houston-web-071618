a13a61a502ab3999691a59e65ad1d9b9eb8a7011
kevinptx <kevin.peery@gmail.com>
2018-08-31 15:33:55 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,29 @@
-class CrewMember {}
+class CrewMember {
+    constructor(position) {
+        this.position = position;
+        this.currentShip = 'Looking for a Rig';
+    }
+    engageWarpDrive() {
+        if ((this.currentShip != 'Looking for a Rig') && (this.position == 'Pilot')) {
+            return this.currentShip.warpDrive = 'engaged';
+        } else {
+            return 'had no effect';
+        }
+    }
+
+    setsInvisibility() {
+        if ((this.currentShip != 'Looking for a Rig') && (this.position == 'Defender')) {
+            return this.currentShip.cloaked = true;
+        } else {
+            return 'had no effect';
+        }
+    }
+
+    chargePhasers() {
+        if ((this.currentShip != 'Looking for a Rig') && (this.position == 'Gunner')) {
+            return this.currentShip.phasers = 'charged';
+        } else {
+            return "had no effect";
+        }
+    }
+}
